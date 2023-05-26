0539f715f5ec014f331977a9dc4a46566bfeec1c
Allen Ng <104080351+allen-ciq@users.noreply.github.com>
2023-05-26 03:20:23 +0000

    Testing multiple actions per yml

--- .github/actions/build-info.sh
+++ .github/actions/build-info.sh
@@ -2,7 +2,7 @@
 # usage: build-info.sh <job name/id> <build number/id>
 
 if [[ $# -ne 2 ]]; then
-		echo 'Usage: build-info.sh <job name/id> <build number/id>'
-			exit 1
+	echo 'Usage: build-info.sh <job name/id> <build number/id>'
+	exit 1
 fi
 echo -e "Source: $(git describe 2> /dev/null || git rev-parse --short HEAD)\nJob: $1\nBuild #: $2\nTime: $(date '+%Y-%m-%d %H:%M:%S')"
