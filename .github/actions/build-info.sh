0f074555419852bfe1946e30c5887a4cf76bbf60
Allen Ng <104080351+allen-ciq@users.noreply.github.com>
2023-05-26 03:12:48 +0000

    Testing build-info script

--- .github/actions/build-info.sh
+++ .github/actions/build-info.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# usage: build-info.sh <job name/id> <build number/id>
 
-echo -e BUILDINFO=Source: $(git describe 2> /dev/null || git rev-parse --short HEAD)\nJob: '${{ github.workflow }}'\nBuild \#: '${{ github.run_number }}'\nTime: $(printf '%(%Y-%m-%d %H:%M:%S)T\n')
+if [[ $# -ne 2 ]]; then
+		echo 'Usage: build-info.sh <job name/id> <build number/id>'
+			exit 1
+fi
+echo -e "Source: $(git describe 2> /dev/null || git rev-parse --short HEAD)\nJob: $1\nBuild #: $2\nTime: $(date '+%Y-%m-%d %H:%M:%S')"
